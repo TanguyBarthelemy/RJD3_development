cca6e52712b571b8c590522deb3f07e525a9273c
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2023-04-28 17:27:32 +0200

    print for calendar

---
+++ Code/new_prints/JD3_WEIGHTEDCALENDAR.R
@@ -0,0 +1,20 @@
+print.JD3_WEIGHTEDCALENDAR_new <- function (x, 
+                                        enable_print_style = getOption("enable_print_style"), 
+                                        ...)
+{
+    if (enable_print_style) {
+        style_pre_code <- "\033[4m\033[1m"
+        style_post_code <- "\033[22m\033[24m"
+    } else {
+        style_pre_code <- style_post_code <- ""
+    }
+    
+    for (index_cal in seq_along(x$weights)) {
+        cat(style_pre_code, "Calendar n°", index_cal, style_post_code, "\n", sep = "")
+        cat("weight: ", x$weight[index_cal], "\n", sep = "")
+        print(x$calendars[[index_cal]])
+        cat("\n")
+    }
+    
+    return(invisible(x))
+}
