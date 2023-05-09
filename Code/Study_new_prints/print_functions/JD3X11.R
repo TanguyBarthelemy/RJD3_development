844daa03787fb02f9c95f7a239510e450832c5c5
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2023-05-09 17:10:19 +0200

    use of the dictionnary to name series

--- Code/Study_new_prints/print_functions/JD3X11.R
+++ Code/Study_new_prints/print_functions/JD3X11.R
@@ -1,9 +1,13 @@
 
 print_JD3X11 <- function(x) {
-    table <- do.call(cbind, x[grepl(pattern = "^d(\\d+)$", x = names(x))])
+    
+    series <- names(x)[toupper(names(x)) %in% dictionnary$name]
+    table_output <- do.call(cbind, x[series])
+    
+    colnames(table_output) <- dictionnary[dictionnary$name %in% toupper(colnames(table_output)), "definition"]
     
     cat("Last values\n")
-    print(tail(.preformat.ts(table)))
+    print(tail(.preformat.ts(table_output)))
     
     return(invisible(x))
 }
