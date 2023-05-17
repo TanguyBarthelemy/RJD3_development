81d9c32ea3c79d6535af6274766a7d7b1f901c10
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2023-05-17 16:19:50 +0200

    restructure code

--- Code/Study_new_prints/error.R
+++ Code/Study_new_prints/error.R
@@ -1,5 +1,7 @@
 ## Error ?
 
+# Les regresseurs n'ont pas de colnames avec q
+
 # spec stock td ---------------------
 
 spec1 <- rjd3x13::spec_x13("rsa3") |> 
@@ -23,15 +25,15 @@ sa_x13_d <- rjd3x13::x13(y_raw, spec_x13_d)
 
 stop("Specific TRAMO...")
 
-spec_x13_d1 <- rjd3x13::spec_x13("rsa5c")
+spec_x13_d1 <- rjd3tramoseats::spec_tramo("trfull")
 spec_x13_d1 <- set_easter(spec_x13_d1, 
                           type = "Standard")
 
-spec_x13_d2 <- rjd3x13::spec_x13("rsa5c")
+spec_x13_d2 <- rjd3tramoseats::spec_tramo("trfull")
 spec_x13_d2 <- set_easter(spec_x13_d2, 
                           type = "IncludeEaster")
 
-spec_x13_d3 <- rjd3x13::spec_x13("rsa5c")
+spec_x13_d3 <- rjd3tramoseats::spec_tramo("trfull")
 spec_x13_d3 <- set_easter(spec_x13_d3, 
                           type = "IncludeEasterMonday")
 
@@ -44,15 +46,15 @@ waldo::compare(spec_x13_d2, spec_x13_d3)
 frenchCalendar <- national_calendar(days = list(
     fixed_day(7, 14), # Fete nationale
     fixed_day(5, 8, validity = list(start = "1982-05-08")), # Victoire 2nd guerre mondiale
-    special_day('NEWYEAR'), # Nouvelle année
-    special_day('CHRISTMAS'), # Noël
-    special_day('MAYDAY'), # 1er mai
-    special_day('EASTERMONDAY'), # Lundi de Pâques
-    special_day('ASCENSION'), # attention +39 et pas 40 jeudi ascension
-    special_day('WHITMONDAY'), # Lundi de Pentecôte (1/2 en 2005 a verif)
-    special_day('ASSUMPTION'), # Assomption
-    special_day('ALLSAINTSDAY'), # Toussaint
-    special_day('ARMISTICE'))
+    special_day("NEWYEAR"), # Nouvelle année
+    special_day("CHRISTMAS"), # Noël
+    special_day("MAYDAY"), # 1er mai
+    special_day("EASTERMONDAY"), # Lundi de Pâques
+    special_day("ASCENSION"), # attention +39 et pas 40 jeudi ascension
+    special_day("WHITMONDAY"), # Lundi de Pentecôte (1/2 en 2005 a verif)
+    special_day("ASSUMPTION"), # Assomption
+    special_day("ALLSAINTSDAY"), # Toussaint
+    special_day("ARMISTICE"))
 )
 
 weighted_cal <- weighted_calendar(list(frenchCalendar, frenchCalendar), c(0.1, 0.5))
