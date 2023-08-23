e8ac909d4f77058141e90cf34cd4e32ca488c4bb
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2023-08-23 09:28:58 +0200

    new work on RJDv2

---
+++ R/nesw function for get_name/get_name_with_pos.R
@@ -0,0 +1,29 @@
+get_name <- function(x, ...){
+    UseMethod("get_name", x)
+}
+#' @export
+get_name.workspace <- function(x, pos){
+    sap_i <- get_object(x, pos = pos)
+    return(get_name(sap_i))
+}
+#' @export
+get_name.multiprocessing <- function(x, pos){
+    if (missing(pos)) {
+        return(.jcall(x, "S", "getName"))
+    } else {
+        sa_item_i <- get_object(x, pos = pos)
+        return(get_name(sa_item_i))
+    }
+}
+#' @export
+get_name.sa_item <- function(x){
+    jt <- .jcall(x, "Ljd2/datatypes/sa/SaItemType;", "getSaDefinition")
+    jts <- .jcall(jt, "Ljd2/datatypes/Ts;", "getTs")
+    name <- .jcall(jts, "S", "getName")
+    # Remove the name of the file link to the saitem
+    name <- gsub("^.*\\n", "", name)
+    return(name)
+}
+
+print(get_name(mp1))
+print(get_name(mp1, 1))
