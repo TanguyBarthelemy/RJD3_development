b3b5176f4e4be652dceddf6ceeb929004759e82f
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-03-22 10:37:42 +0100

    update code HF

--- R/demo/demo_rjd3highfreq.R
+++ R/demo/demo_rjd3highfreq.R
@@ -5,18 +5,22 @@
 
 # Chargement des packages ------------------------------------------------------
 
+# La PR n'a pas été accepté donc il faut installer mon package rjd3toolkit
+
+remotes::install_github("TanguyBarthelemy/rjd3toolkit")
+
 library("rjd3highfreq")
 library("ggplot2")
+library("rjd3toolkit")
 
 
-# Chargement data --------------------------------------------------
+# Chargement data --------------------------------------------------------------
 
 df_daily <- read.csv("./data/TS_daily_births_franceM_1968_2020.csv", sep = ";") |>
     dplyr::mutate(
         log_births = log(births),
         date = as.Date(date)
-    ) |>
-    head(8000)
+    )
 
 # This dataframe contains the following variables:
 # date       = from 01/01/1968 to 12/31/2000
@@ -25,46 +29,9 @@ df_daily <- read.csv("./data/TS_daily_births_franceM_1968_2020.csv", sep = ";") 
 # day        = indicates the day of the week, D1=Monday...D7=Sunday
 # month      = indicates the day of the month, M01=January...M12=December
 
-ch.sp <- 2:367 # Seasonal periodicities to be tested for
 
-df_ch <- data.frame(
-    "sp" = ch.sp,
-    "ch.raw" = rjd3toolkit::seasonality_canovahansen(
-        data = df_daily$births,
-        p0 = min(ch.sp), p1 = max(ch.sp),
-        np = max(ch.sp) - min(ch.sp) + 1L, original = TRUE
-    ),
-    "ch.log" = rjd3toolkit::seasonality_canovahansen(
-        data = df_daily$log_births,
-        p0 = min(ch.sp), p1 = max(ch.sp),
-        np = max(ch.sp) - min(ch.sp) + 1L, original = TRUE
-    ),
-    "ch.dlg" = rjd3toolkit::seasonality_canovahansen(
-        data = diff(df_daily$log_births, lag = 1L, differences = 1L),
-        p0 = min(ch.sp), p1 = max(ch.sp),
-        np = max(ch.sp) - min(ch.sp) + 1L, original = TRUE
-    )
-)
+# Calendar regressors ----------------------------------------------------------
 
-# Significant periodicities (Harvey, 2001, Table I(b))
-
-which(df_ch$ch.raw > .211) + 1 # 10% level of significance
-which(df_ch$ch.raw > .247) + 1 #  5% level of significance
-which(df_ch$ch.raw > .329) + 1 #  1% level of significance
-
-# Barplot
-
-ggplot(df_ch) +
-    geom_col(aes(sp, ch.raw), linewidth = .25) +
-    labs(x = "Periodicity (in days)", y = "") +
-    ggthemes::theme_hc()
-
-# -------------------------------------------------------------------------------------------------
-# (3) Calendar regressors
-# -------------------------------------------------------------------------------------------------
-
-library("rjd3toolkit")
-library("rjd3highfreq")
 
 # Names for calendar regressors
 calendar_regressors <- c(
@@ -100,106 +67,143 @@ q <- holidays(
 )
 
 
-# -------------------------------------------------------------------------------------------------
-# (4) Pre-adjustement with extended fractional Airline Model
-# -------------------------------------------------------------------------------------------------
+# Muti AMB decomposition with extended fractional Airline Model ----------------
 
-library("dplyr")
 
-# Reg-Arima estimation
-
-pre.mult_bl <- fractionalAirlineEstimation(
-    y = df_daily$log_births,
-    y_time = seq.Date(from = as.Date("1968-01-01"), length.out = length(df_daily$log_births), by = "days"),
-    x = q, # q= calendar regressors matrix
-    periods = c(7, 365.25),
+f <- function(
+    # input time series
+    y,
+    # Different periods
+    periods,
+    # q= calendar regressors matrix
+    x = NULL,
     ndiff = 2,
     ar = FALSE,
     mean = FALSE,
-    outliers = c("ao", "wo"), # type of outliers detected
-    criticalValue = 0, # automatically set
-    precision = 1e-9,
-    approximateHessian = TRUE
-)
-pre.mult_log <- fractionalAirlineEstimation(
+    # type of outliers detected
+    outliers = c("ao", "wo"),
+    criticalValue = 0,
+    precision = 1e-12,
+    approximateHessian = FALSE,
+    # Signal (SA)-noise decomposition
+    sn = FALSE,
+    # Calculate standard deviations
+    stde = FALSE,
+    # Numbers of back- and forecasts
+    nbcasts = 0,
+    nfcasts = 0,
+    # To perform a multiplicative model
+    log = FALSE,
+    y_time = NULL
+) {
+    if (is.null(y_time) && !is.null(x)) {
+        y_time <- rownames(x)
+    }
+
+    pre_adj <- fractionalAirlineEstimation(
+        y = y,
+        y_time = y_time,
+        x = x,
+        periods = periods,
+        ndiff = ndiff,
+        ar = ar,
+        mean = mean,
+        outliers = outliers,
+        criticalValue = criticalValue,
+        precision = precision,
+        approximateHessian = approximateHessian,
+        log = log
+    )
+
+    if (!is.null(pre_adj$model$variables)) {
+        # Retrieving estimated outlier & calendar effects (coefs, SE, student)
+        regs <- data.frame(
+            "Variable" = pre_adj$model$variables,
+            "Coef"     = pre_adj$model$b,
+            "Coef_SE"  = sqrt(diag(pre_adj$model$bcov))
+        )
+        regs$Tstat <- round(regs$Coef / regs$Coef_SE, 2)
+        pre_adj$model <- c(pre_adj$model, list(regressors_coefficients = regs))
+    }
+
+    # Retrieving estimated MA parameters (coefs, se, student)
+    MA_coeffs <- data.frame(
+        "MA parameter" = c("Theta(1)", paste0("Theta(period = ", pre_adj$model$periods, ")")),
+        "Coef" = pre_adj$estimation$parameters,
+        "Coef_SE" = sqrt(diag(pre_adj$estimation$covariance)),
+        check.names = FALSE
+    )
+    MA_coeffs$Tstat <- MA_coeffs$Coef / MA_coeffs$Coef_SE
+    pre_adj$model <- c(pre_adj$model, list(sarima_coefficients = MA_coeffs))
+
+    sa <- pre_adj$model$linearized
+    all_amb <- list()
+    for (p in sort(periods)) {
+        amb <- rjd3highfreq::fractionalAirlineDecomposition(
+            y = sa,
+            period = p,
+            sn = sn,
+            stde = stde,
+            nbcasts = nbcasts,
+            nfcasts = nfcasts,
+            log = log,
+            y_time = y_time
+        )
+        all_amb <- c(all_amb, list(amb))
+        sa <- amb$decomposition$sa
+    }
+
+    # Compute final components and SA series
+    #calendar component
+    cal.cmp <- rep(0, length(y))
+    if (!is.null(x)) {
+        cal.cmp <- pre_adj$model$xreg[, seq_len(ncol(x))] %*% pre_adj$model$b[seq_len(ncol(x))]
+    }
+
+    #final s components
+    amb_s <- lapply(all_amb, \(.amb) .amb$decomposition$s)
+    amb_s <- as.data.frame(setNames(amb_s, paste0("s_", sort(periods))))
+
+    #final sa
+    y_trans <- y
+    if (log) y_trans <- log(y)
+    amb.sa <- y_trans - (cal.cmp + rowSums(amb_s))
+
+    # group
+    final_decomposition <- data.frame(
+        y = y_trans,
+        lin = pre_adj$model$linearized,
+        cal = cal.cmp,
+        amb_s,
+        sa = amb.sa
+    )
+    rownames(final_decomposition) <- as.character(y_time)
+    if (log) final_decomposition <- exp(final_decomposition)
+
+    output <- c(
+        list(preprocessing = pre_adj),
+        setNames(all_amb, paste0("amb_model_period_", sort(periods))),
+        list(final = final_decomposition)
+    )
+
+    return(output)
+}
+
+out <- f(
     y = df_daily$births,
-    y_time = seq.Date(from = as.Date("1968-01-01"), length.out = length(df_daily$log_births), by = "days"),
-    x = q, # q= calendar regressors matrix
     periods = c(7, 365.25),
+    x = q,
     ndiff = 2,
     ar = FALSE,
     mean = FALSE,
-    outliers = c("ao", "wo"), # type of outliers detected
-    criticalValue = 0, # automatically set
+    outliers = c("ao", "wo"),
+    criticalValue = 0,
     precision = 1e-9,
     approximateHessian = TRUE,
-    log = TRUE
-)
-
-# Retrieving estimated outlier & calendar effects (coefs, se, student)
-
-regs_mult <- data.frame(
-    "Variable" = pre.mult$model$variables,
-    "Coef"     = pre.mult$model$b,
-    "Coef_SE"  = sqrt(diag(pre.mult$model$bcov))
-) |> mutate(Tstat = round(Coef / Coef_SE, 2))
-
-
-# Retrieving estimated MA parameters (coefs, se, student)
-
-MA_coeffs <- data.frame(
-    "MA parameter" = c("Theta(1)", paste0("Theta(period = ", pre.mult$model$periods, ")")),
-    "Coef" = pre.mult$estimation$parameters,
-    "Coef_SE" = sqrt(diag(pre.mult$estimation$covariance)),
-    check.names = FALSE
-) |>
-    mutate(Tstat = Coef / Coef_SE)
-
-# -------------------------------------------------------------------------------------------------
-# (5) AMB decomposition with extended fractional Airline Model
-# -------------------------------------------------------------------------------------------------
-#
-# Extract DOW pattern from linearised series
-
-amb.dow <- rjd3highfreq::fractionalAirlineDecomposition(
-    pre.mult$model$linearized, # input time series
-    period = 7, # DOW pattern
-    sn = FALSE, # Signal (SA)-noise decomposition
-    stde = FALSE, # Calculate standard deviations
-    nbcasts = 0, nfcasts = 0
-) # Numbers of back- and forecasts
-
-# Extract DOY pattern from DOW-adjusted linearised data
-amb.doy <- rjd3highfreq::fractionalAirlineDecomposition(
-    y = amb.dow$decomposition$sa, # DOW-adjusted linearised data
-    period = 365.2425, # DOY pattern
     sn = FALSE,
     stde = FALSE,
-    nbcasts = 0, nfcasts = 0
+    nbcasts = 0,
+    nfcasts = 0,
+    log = TRUE,
+    y_time = seq.Date(from = as.Date("1968-01-01"), length.out = length(df_daily$log_births), by = "days")
 )
-
-
-# Compute final components and SA series
-
-
-# calendar component
-df_daily <- df_daily |>
-    mutate(cal.cmp = exp(pre.mult$model$xreg[, 1:length(calendar_regressors)] %*%
-                             pre.mult$model$b[1:length(calendar_regressors)]))
-
-# final dow, doy and sa
-df_daily <- df_daily |>
-    mutate(amb.dow = exp(amb.dow$decomposition$s)) |>
-    mutate(amb.doy = exp(amb.doy$decomposition$s)) |>
-    mutate(amb.sa = births / (cal.cmp * amb.dow * amb.doy))
-
-head(df_daily)
-head(calendar_regressors)
-
-# Plot raw and SA series
-ggplot(df_daily) +
-    geom_line(aes(date, births), size = .000001) +
-    geom_line(aes(date, amb.sa), size = .000001, col = "red") +
-    labs(x = "", y = "") +
-    theme_classic() +
-    theme(panel.grid.major.y = element_line(color = "black", size = 0.5))
--- R/other/sa_hf.R
+++ R/other/sa_hf.R
@@ -1,52 +1,209 @@
-f <- function(
-        y,
-        periods,
-        x = NULL,
-        ndiff = 2,
-        ar = FALSE,
-        mean = FALSE,
-        outliers = NULL,
-        criticalValue = 6,
-        precision = 1e-12,
-        approximateHessian = FALSE,
-        sn = FALSE,
-        stde = FALSE,
-        nbcasts = 0,
-        nfcasts = 0,
-        log = FALSE,
-        y_time = NULL
-) {
+################################################################################
+#######                Démonstration de rjd3highfreq                     #######
+################################################################################
 
-    pre_adj <- rjd3highfreq::fractionalAirlineEstimation(
+
+# Chargement des packages ------------------------------------------------------
+
+# La PR n'a pas été accepté donc il faut installer mon package rjd3toolkit
+
+remotes::install_github("TanguyBarthelemy/rjd3toolkit")
+
+library("rjd3highfreq")
+library("ggplot2")
+library("rjd3toolkit")
+
+
+# Chargement data --------------------------------------------------------------
+
+df_daily <- read.csv("./data/TS_daily_births_franceM_1968_2020.csv", sep = ";") |>
+    dplyr::mutate(
+        log_births = log(births),
+        date = as.Date(date)
+    )
+
+# This dataframe contains the following variables:
+# date       = from 01/01/1968 to 12/31/2000
+# births     = daily number of French Births
+# log_births = daily number of French Births in log
+# day        = indicates the day of the week, D1=Monday...D7=Sunday
+# month      = indicates the day of the month, M01=January...M12=December
+
+
+# Calendar regressors ----------------------------------------------------------
+
+
+# Names for calendar regressors
+calendar_regressors <- c(
+    "Bastille_day", "Victory_day", "NEWYEAR", "CHRISTMAS", "MAYDAY",
+    "EASTERMONDAY", "ASCENSION", "WHITMONDAY", "ASSUMPTION", "ALLSAINTSDAY",
+    "ARMISTICE"
+)
+
+# Define a national calendar
+french_calendar <- national_calendar(
+    days = list(
+        Bastille_day = fixed_day(7, 14), # Bastille Day
+        Victory_day = fixed_day(5, 8, validity = list(start = "1982-05-08")), # Victory Day
+        NEWYEAR = special_day("NEWYEAR"),
+        CHRISTMAS = special_day("CHRISTMAS"),
+        MAYDAY = special_day("MAYDAY"),
+        EASTERMONDAY = special_day("EASTERMONDAY"),
+        ASCENSION = special_day("ASCENSION"),
+        WHITMONDAY = special_day("WHITMONDAY"),
+        ASSUMPTION = special_day("ASSUMPTION"),
+        ALLSAINTSDAY = special_day("ALLSAINTSDAY"),
+        ARMISTICE = special_day("ARMISTICE")
+    )
+)
+
+# Generrate calendar regressors
+q <- holidays(
+    calendar = french_calendar,
+    start = "1968-01-01",
+    length = length(df_daily$births),
+    type = "All",
+    nonworking = 7L
+)
+
+
+# Muti AMB decomposition with extended fractional Airline Model ----------------
+
+
+f <- function(
+        # input time series
+    y,
+    # Different periods
+    periods,
+    # q= calendar regressors matrix
+    x = NULL,
+    ndiff = 2,
+    ar = FALSE,
+    mean = FALSE,
+    # type of outliers detected
+    outliers = c("ao", "wo"),
+    criticalValue = 0,
+    precision = 1e-12,
+    approximateHessian = FALSE,
+    # Signal (SA)-noise decomposition
+    sn = FALSE,
+    # Calculate standard deviations
+    stde = FALSE,
+    # Numbers of back- and forecasts
+    nbcasts = 0,
+    nfcasts = 0,
+    # To perform a multiplicative model
+    log = FALSE,
+    y_time = NULL
+) {
+    if (is.null(y_time) && !is.null(x)) {
+        y_time <- rownames(x)
+    }
+
+    pre_adj <- fractionalAirlineEstimation(
         y = y,
         y_time = y_time,
-        x = q, # q= calendar regressors matrix
+        x = x,
         periods = periods,
-        ndiff = 2,
-        ar = FALSE,
-        mean = FALSE,
-        outliers = c("ao", "wo"), # type of outliers detected
-        criticalValue = 0, # automatically set
-        precision = 1e-9,
-        approximateHessian = TRUE,
+        ndiff = ndiff,
+        ar = ar,
+        mean = mean,
+        outliers = outliers,
+        criticalValue = criticalValue,
+        precision = precision,
+        approximateHessian = approximateHessian,
         log = log
     )
 
-    sa <- pre_adj$model$linearized
+    if (!is.null(pre_adj$model$variables)) {
+        # Retrieving estimated outlier & calendar effects (coefs, SE, student)
+        regs <- data.frame(
+            "Variable" = pre_adj$model$variables,
+            "Coef"     = pre_adj$model$b,
+            "Coef_SE"  = sqrt(diag(pre_adj$model$bcov))
+        )
+        regs$Tstat <- round(regs$Coef / regs$Coef_SE, 2)
+        pre_adj$model <- c(pre_adj$model, list(regressors_coefficients = regs))
+    }
 
+    # Retrieving estimated MA parameters (coefs, se, student)
+    MA_coeffs <- data.frame(
+        "MA parameter" = c("Theta(1)", paste0("Theta(period = ", pre_adj$model$periods, ")")),
+        "Coef" = pre_adj$estimation$parameters,
+        "Coef_SE" = sqrt(diag(pre_adj$estimation$covariance)),
+        check.names = FALSE
+    )
+    MA_coeffs$Tstat <- MA_coeffs$Coef / MA_coeffs$Coef_SE
+    pre_adj$model <- c(pre_adj$model, list(sarima_coefficients = MA_coeffs))
+
+    sa <- pre_adj$model$linearized
+    all_amb <- list()
     for (p in sort(periods)) {
         amb <- rjd3highfreq::fractionalAirlineDecomposition(
-            y = sa, # input time series
-            period = p, # DOW pattern
-            sn = FALSE, # Signal (SA)-noise decomposition
-            stde = FALSE, # Calculate standard deviations
-            nbcasts = 0, nfcasts = 0,
-            log = log, y_time = y_time
-        ) # Numbers of back- and forecasts
+            y = sa,
+            period = p,
+            sn = sn,
+            stde = stde,
+            nbcasts = nbcasts,
+            nfcasts = nfcasts,
+            log = log,
+            y_time = y_time
+        )
+        all_amb <- c(all_amb, list(amb))
         sa <- amb$decomposition$sa
     }
 
-    # ici faire des calculs, des regroupements pour conserver des éléments du pre_adj et de TOUS les amb et pas seulement le dernier
+    # Compute final components and SA series
+    #calendar component
+    cal.cmp <- rep(0, length(y))
+    if (!is.null(x)) {
+        cal.cmp <- pre_adj$model$xreg[, seq_len(ncol(x))] %*% pre_adj$model$b[seq_len(ncol(x))]
+    }
 
-    return(amb)
+    #final s components
+    amb_s <- lapply(all_amb, \(.amb) .amb$decomposition$s)
+    amb_s <- as.data.frame(setNames(amb_s, paste0("s_", sort(periods))))
+
+    #final sa
+    y_trans <- y
+    if (log) y_trans <- log(y)
+    amb.sa <- y_trans - (cal.cmp + rowSums(amb_s))
+
+    # group
+    final_decomposition <- data.frame(
+        y = y_trans,
+        lin = pre_adj$model$linearized,
+        cal = cal.cmp,
+        amb_s,
+        sa = amb.sa
+    )
+    rownames(final_decomposition) <- as.character(y_time)
+    if (log) final_decomposition <- exp(final_decomposition)
+
+    output <- c(
+        list(preprocessing = pre_adj),
+        setNames(all_amb, paste0("amb_model_period_", sort(periods))),
+        list(final = final_decomposition)
+    )
+
+    return(output)
 }
+
+out <- f(
+    y = df_daily$births,
+    periods = c(7, 365.25),
+    x = q,
+    ndiff = 2,
+    ar = FALSE,
+    mean = FALSE,
+    outliers = c("ao", "wo"),
+    criticalValue = 0,
+    precision = 1e-9,
+    approximateHessian = TRUE,
+    sn = FALSE,
+    stde = FALSE,
+    nbcasts = 0,
+    nfcasts = 0,
+    log = TRUE,
+    y_time = seq.Date(from = as.Date("1968-01-01"), length.out = length(df_daily$log_births), by = "days")
+)
