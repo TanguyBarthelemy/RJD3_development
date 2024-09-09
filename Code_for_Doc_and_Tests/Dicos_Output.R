f98de8afff0e13a2b203333ef61b5f3399947ef6
Anna SMYK <anna.smyk@insee.fr>
2024-09-09 09:38:19 +0200

    update

---
+++ Code_for_Doc_and_Tests/Dicos_Output.R
@@ -0,0 +1,14 @@
+### Dictionnaries x13, ts, toolkit
+library(rjd3toolkit)
+library(rjd3x13)
+library(rjd3tramoseats)
+library(xlsx)
+
+a<-rjd3x13::x13_full_dictionary() |> as.data.frame()
+write.xlsx(a,"x13dico.xlsx")
+
+b<-rjd3tramoseats::tramoseats_full_dictionary() |> as.data.frame()
+write.xlsx(b,"TSdico.xlsx")
+
+# toolkit
+# rjd3toolkit::.r2jd_modellingcontext(my_context)$getCalendars()$getNames()
